95ce567d8994da448c32d797a00d021c160cc05b
marjanak <marjana1816@gmail.com>
2024-11-15 09:35:53 -0800

    Completed wave 1 and 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,31 @@
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self,inventory=None):
+        self.inventory=inventory
+        self.inventory= [] if self.inventory is None else self.inventory
+        
+    def add(self,item):
+        
+        self.inventory.append(item)
+        return item
+    
+    def remove(self,item):
+    
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+        return item
+    
+    def get_by_id (self,item_id):
+        for item in self.inventory:
+            if item.id == item_id:
+                return item
+            else:
+                return None
+
+
+
+    
+
