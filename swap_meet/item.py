be95390d18eae5fda2602d8389edd4768cd68670
marjanak <marjana1816@gmail.com>
2024-12-20 01:11:47 -0800

    Done with wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,12 +1,29 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         self.id = uuid.uuid4().int if id == None else id
+        self.condition = condition
+    
     def __str__(self):
         return f"An object of type Item with id {self.id}"
     
        
     def get_category(self):
         return "Item"
+    
+    def condition_description(self):
+        if self.condition == 5:
+            return "Excellent"
+        elif self.condition == 4:
+            return "Very Good"
+        elif self.condition == 3:
+            return "Good"
+        elif self.condition == 2:
+            return "Okay"
+        elif self.condition == 1:
+            return "Bad"
+
+
+
         
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,7 +2,7 @@ from swap_meet.item import Item
 
 class Vendor:
     def __init__(self,inventory=None):
-        self.inventory=inventory
+        self.inventory= inventory
         self.inventory= [] if self.inventory is None else self.inventory
         
     def add(self,item):
@@ -29,15 +29,29 @@ class Vendor:
         for item in self.inventory:
             if item not in self.inventory:
                 return False
+            elif their_item not in other_vendor.inventory:
+                return False
             else:
-                self.remove(my_item)
-                other_vendor.add(my_item)
-                other_vendor.remove(their_item)
-                self.add(their_item)
+                other_vendor.inventory.append(my_item)
+                self.inventory.append(their_item)
+                self.inventory.remove(my_item)
+                other_vendor.inventory.remove(their_item)
             return True
         
+    def swap_first_item(self, other_vendor):
+        if len(self.inventory) > 0 and len(other_vendor.inventory) > 0:
+            first_item = self.inventory[0]
+            other_first_item = other_vendor.inventory[0]
 
+            self.inventory.remove(first_item)
+            other_vendor.inventory.append(first_item)
+            other_vendor.inventory.remove(other_first_item)
+            self.inventory.append(other_first_item)
 
+            return True
+        else:
+            return False
+        
 
 
 
